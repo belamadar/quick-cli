d3e5ff50e6a58797f75761e28d16831b827b0665
belamadar <bikkicsongor@pm.me>
2025-09-25 12:04:46 +0100

    Unkown updates?

--- install.sh
+++ install.sh
@@ -16,9 +16,16 @@ elif [ -x /opt/homebrew/bin/brew ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
+msg "Checking for ZShell..."
+if which zsh >/dev/null 2>&1; then
+	msg "ZShell is available"
+else
+	msg "ZShell is not available"
+	brew install zsh
+fi	
+
 msg "Installing terminal tools..."
 brew install \
-  zsh \
   zsh-autosuggestions zsh-syntax-highlighting zsh-completions \
   zoxide \
   starship \
@@ -34,7 +41,7 @@ msg "Installing Nerd Font..."
 if [ ! -f ~/.local/share/fonts/CaskaydiaCoveNerdFont-Regular.ttf ]; then
   brew install --cask font-caskaydia-cove-nerd-font
 else
-  echo ">>> Nerd Font already installed, skipping."
+  echo "Nerd Font already installed, skipping."
 fi
 
 msg "Setting up zsh config..."
@@ -63,7 +70,7 @@ SAVEHIST=5000
 # Aliases
 alias ls="eza --icons --group-directories-first"
 alias cat="bat"
-alias vim="nvim"
+alias vi="nvim"
 
 # Plugins
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
@@ -78,33 +85,11 @@ eval "$(fzf --zsh)"
 # Prompt
 eval "$(starship init zsh)"
 
-# MOTD
-if [ -f ~/.config/quick-cli/motd ]; then
-  cat ~/.config/quick-cli/motd
-fi
 EOF
 
-# Create MOTD file
-mkdir -p ~/.config/quick-cli
-/bin/cat > ~/.config/quick-cli/motd <<'EOM'
-
-  󱍢 Welcome to Quick CLI
-
-   $(fastfetch --structure User:Host:Shell:OS)
-
-    Command                          │ Description
-  ────────────────────────────────────┼───────────────────────────────────────────────
-    brew help                         │ Manage command line packages
-    tldr <cmd>                        │ Show simplified man pages
-    zoxide query <dir>                │ Jump quickly to a directory
-    fzf                               │ Fuzzy-find files or history
-    vi                                │ Open the modern neovim text editor
-
-EOM
 
 msg "Applying Starship preset..."
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
 
 msg "Done! Restart your terminal to enjoy zsh + starship."
-echo "To make zsh your default shell, run: chsh -s $(which zsh)"
 
